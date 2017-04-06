2bd1b56ffe1de4eef383ebc094c93d01a45b3dc8
Patrick McGreevy <patrick7490@icloud.com>
2017-04-06 15:40:38 -0700

    Remove id property from album

--- src/app/album.model.ts
+++ src/app/album.model.ts
@@ -2,7 +2,6 @@ export class Album {
   constructor(
     public title: string,
     public artist: string,
-    public description: string,
-    public id: number
+    public description: string
   ) { }
 }
--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
 
 @Injectable()
 export class AlbumService {
@@ -16,13 +15,15 @@ export class AlbumService {
     return this.albums;
   }
 
-  getAlbumById(id: number) {
-    for (let i = 0; i < ALBUMS.length; i++) {
-      let album: Album = ALBUMS[i];
-      if (album.id  === id) {
-        return album;
-      }
-    }
   }
+
+  // getAlbumById(id: number) {
+  //   for (let i = 0; i < ALBUMS.length; i++) {
+  //     let album: Album = ALBUMS[i];
+  //     if (album.id  === id) {
+  //       return album;
+  //     }
+  //   }
+  // }
 
 }
